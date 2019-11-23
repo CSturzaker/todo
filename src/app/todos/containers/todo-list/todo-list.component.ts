1831ec5b9c9a1c853a3f4c4043ee207ba1983656
CSturzaker <craig_sturzaker@hotmail.com>
2019-11-23 20:44:51 +0000

    todo list and header components

--- src/app/todos/containers/todo-list/todo-list.component.ts
+++ src/app/todos/containers/todo-list/todo-list.component.ts
@@ -1,12 +1,40 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ChangeDetectionStrategy } from '@angular/core'
+import { Store, select } from '@ngrx/store'
+
+import { Observable } from 'rxjs'
+
+import * as TodoActions from '../../actions/todo.actions'
+
+import { Todo } from '../../models'
+
+import * as fromTodos from '../../reducers'
 
 @Component({
   selector: 'todo-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodoListComponent implements OnInit {
-  constructor() {}
+export class TodoListComponent {
+  todoItems$: Observable<Todo[]> = this.store.pipe(
+    select(fromTodos.selectTodos)
+  )
+  newTodo = ''
 
-  ngOnInit() {}
+  constructor(private store: Store<fromTodos.State>) {}
+
+  addTodo() {
+    this.store.dispatch(TodoActions.addTodo(this.newTodo))
+    this.newTodo = ''
+  }
+
+  toggleTodo(todo: Todo) {
+    const { id } = todo
+    this.store.dispatch(TodoActions.toggleTodo({ id }))
+  }
+
+  removeTodo(todo: Todo) {
+    const { id } = todo
+    this.store.dispatch(TodoActions.removeTodo({ id }))
+  }
 }
