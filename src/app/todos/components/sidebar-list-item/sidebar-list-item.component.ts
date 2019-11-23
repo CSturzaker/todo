7b664fe8cbcdcc2c58babcd6d066646e0221c978
CSturzaker <craig_sturzaker@hotmail.com>
2019-11-23 14:51:55 +0000

    scaffold sidebar components

---
+++ src/app/todos/components/sidebar-list-item/sidebar-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'todo-sidebar-list-item',
+  templateUrl: './sidebar-list-item.component.html',
+  styleUrls: ['./sidebar-list-item.component.scss'],
+})
+export class SidebarListItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
