eff919bb15a5e406aaadff0c9237e84e1a467bcc
CSturzaker <craig_sturzaker@hotmail.com>
2019-11-23 14:51:55 +0000

    scaffold sidebar components

---
+++ src/app/todos/components/sidebar-header/sidebar-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'todo-sidebar-header',
+  templateUrl: './sidebar-header.component.html',
+  styleUrls: ['./sidebar-header.component.scss'],
+})
+export class SidebarHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
+import { MaterialModule } from '../material/material.module'
 import { TodosRoutingModule } from './todos-routing.module'
+
 import { TodosPageComponent } from './containers/todos-page/todos-page.component'
+import { SidebarComponent } from './containers/sidebar/sidebar.component'
+import { SidebarHeaderComponent } from './components/sidebar-header/sidebar-header.component'
+import { SidebarListItemComponent } from './components/sidebar-list-item/sidebar-list-item.component'
 
 @NgModule({
-  declarations: [TodosPageComponent],
-  imports: [CommonModule, TodosRoutingModule],
+  declarations: [
+    TodosPageComponent,
+    SidebarComponent,
+    SidebarHeaderComponent,
+    SidebarListItemComponent,
+  ],
+  imports: [CommonModule, MaterialModule, TodosRoutingModule],
 })
 export class TodosModule {}
